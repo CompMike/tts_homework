1447b775d56da6767f572b8345952330fac5af1b
Michael Heard <compmike@gmail.com>
2014-07-31 20:48:51 -0400

    fixed error in code and added a test to see if Y or N is entered

--- homework2/fivequestions.rb
+++ homework2/fivequestions.rb
@@ -4,7 +4,7 @@
 puts "It's time for the 5 questions game! You Ready?\n"
 puts "Instructions: \nAnswer all questions, your score will be output at the end of the game.\n"
 #define the questions and the answers
-questions = [["Is the sky blue?","Y"],["Is it summer?","Y"],["Is the Whitehouse green?","N"]["Is this a great day?","N"]["Is it good to be alive?","N"]]
+questions = [["Is the sky blue?","Y"],["Is it summer?","Y"],["Is the Whitehouse green?","N"],["Is this a great day?","N"],["Is it good to be alive?","N"]]
 score = 0 #running count of score
 #output questions and collect the answer
 questions.each do |q|
@@ -12,6 +12,9 @@ questions.each do |q|
 	answer = gets.chomp
 	if q[1] == answer.upcase
 		score +=1
+	elsif answer.upcase != ("Y"||"N")
+		puts "Please enter Y or N"
+		answer = gets.chomp
 	end
 end
 puts "Your score: #{score} out of #{questions.length}"
