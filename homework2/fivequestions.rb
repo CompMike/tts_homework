50939dde8eb2541da38da3b52a7e6dec579c8d9a
Michael Heard <compmike@gmail.com>
2014-08-03 10:20:01 -0400

    Modified answer testing code to account for lowercase answers, removed unused code.

--- homework2/fivequestions.rb
+++ homework2/fivequestions.rb
@@ -6,18 +6,17 @@ puts "Instructions: \nAnswer all questions with Y or N, your score will be outpu
 #define the questions and the answers
 questions = [["Is the sky blue?","Y"],["Is it summer?","Y"],["Is the Whitehouse green?","N"],["Is this a great day?","N"],["Is it good to be alive?","N"]]
 score = 0 #running count of score
-answer = "" #define the answer
 #output questions and collect the answer
 questions.each do |q|
 	puts q[0]
 	answer = gets.chomp
-	while answer != "Y" and answer != "N" do
+	while answer.upcase != "Y" and answer.upcase != "N" do
 		puts "Please enter Y or N only"
 		answer = gets.chomp
 	end
 	if q[1] == answer.upcase
 			score +=1
-		end
+	end
 end
 puts "Your score: #{score} out of #{questions.length}"
 
