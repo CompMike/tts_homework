e3592281366730f617da5b2497622faf6e016b08
Michael Heard <compmike@gmail.com>
2014-08-03 10:13:29 -0400

    Fixed Y or N test to keep asking if another answer is entered.

--- homework2/fivequestions.rb
+++ homework2/fivequestions.rb
@@ -2,20 +2,22 @@
 #create a 5 questions game, that asks a question, collects the answer, compares it to my answers and outputs a score
 #Introduction to the game.
 puts "It's time for the 5 questions game! You Ready?\n"
-puts "Instructions: \nAnswer all questions, your score will be output at the end of the game.\n"
+puts "Instructions: \nAnswer all questions with Y or N, your score will be output at the end of the game."
 #define the questions and the answers
 questions = [["Is the sky blue?","Y"],["Is it summer?","Y"],["Is the Whitehouse green?","N"],["Is this a great day?","N"],["Is it good to be alive?","N"]]
 score = 0 #running count of score
+answer = "" #define the answer
 #output questions and collect the answer
 questions.each do |q|
 	puts q[0]
 	answer = gets.chomp
-	if q[1] == answer.upcase
-		score +=1
-	elsif answer.upcase != ("Y"||"N")
-		puts "Please enter Y or N"
+	while answer != "Y" and answer != "N" do
+		puts "Please enter Y or N only"
 		answer = gets.chomp
 	end
+	if q[1] == answer.upcase
+			score +=1
+		end
 end
 puts "Your score: #{score} out of #{questions.length}"
 
